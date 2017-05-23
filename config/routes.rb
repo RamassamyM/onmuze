66084dd9771c2dadf0de29ba95ed8e1aae344dc5
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-23 12:31:25 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :performances, only: %i(index show)
   resources :places, only: %i(index show), shallow: true do
-    resources :events, only: %i(index show new crete)
+    resources :events, only: %i(index show new create)
   end
   resources :places, only: %i(new create)
   root to: 'pages#home'
