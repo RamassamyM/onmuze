14fdb3577a40d4a9d267f515351803f496988f4b
Pierre Paillere <peyop78@gmail.com>
2017-05-23 16:09:10 +0200

    performances#index + fixed bug in routes

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,5 +6,5 @@ class Place < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   geocoded_by :address
-  after_validation :geocode, if: :address_changed
+  after_validation :geocode, if: :address_changed?
 end
