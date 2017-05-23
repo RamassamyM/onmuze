dddf1dba83433927b03216d239327cbbe0834935
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-23 11:30:00 +0200

    Fixed schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 20170523085520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
-  create_table "places", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.text     "description"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_places_on_user_id", using: :btree
-
   create_table "performances", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "category"
@@ -38,6 +28,16 @@ ActiveRecord::Schema.define(version: 20170523085520) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.index ["user_id"], name: "index_performances_on_user_id", using: :btree
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_places_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,6 +57,6 @@ ActiveRecord::Schema.define(version: 20170523085520) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-
-  add_foreign_key "places", "users"
   add_foreign_key "performances", "users"
+  add_foreign_key "places", "users"
+end
