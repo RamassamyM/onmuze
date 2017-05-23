348679c09f5b5a3d909932343c4dc4a09335c253
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-23 11:12:27 +0200

    Added places model

---
+++ app/models/place.rb
@@ -0,0 +1,5 @@
+class Place < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
+  validates :address, presence: true
+end
---
+++ db/migrate/20170523085520_create_places.rb
@@ -0,0 +1,12 @@
+class CreatePlaces < ActiveRecord::Migration[5.0]
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
