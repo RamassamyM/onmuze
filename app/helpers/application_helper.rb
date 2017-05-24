61ac4baa79dac117d7a59f3ba533f20dca7b8528
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-24 16:05:51 +0200

    Added sign up modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def complete_profile?
+    user_not_prompted = user_signed_in? && current_user.places.empty? && current_user.performances.empty? && !session[:fully_registered]
+    session[:fully_registered] = true if user_not_prompted
+    return user_not_prompted
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :performances, only: %i(index show)
+  resources :performances, only: %i(index show new create)
   resources :places, only: %i(index show new create), shallow: true do
     resources :events, only: %i(index show new create)
   end
