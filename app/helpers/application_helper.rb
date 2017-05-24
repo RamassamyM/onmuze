9926b004f9c6c588d14a025d007faff14a8fc9cb
Louise Devilder <devilderlouise@gmail.com>
2017-05-24 11:42:04 +0200

    create helper to diplay defaut or not photo into banner + update in places#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def place_photo(place)
+    if place.photo.present?
+      cl_image_path place.photo.path, crop: :fill
+    else
+      asset_path 'places.jpg'
+    end
+  end
+
 end
