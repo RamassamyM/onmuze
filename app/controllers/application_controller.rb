d4968399a73a206b3e31477239b93fc9b4275da3
Michael Ramassamy <ramassamy2000@gmail.com>
2017-05-25 16:57:21 +0200

    Fix redirection after signup or login to the previous page or to the modal to create a performance or a place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :store_current_location, :unless => :devise_controller?
 
   private
 
@@ -8,4 +9,12 @@ class ApplicationController < ActionController::Base
     session[:fully_registered] = false
     root_path
   end
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
 end
+
+
+
+
