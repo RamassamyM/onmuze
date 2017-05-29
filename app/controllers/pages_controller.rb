ebfacfeac55fb69812a0722f797e7d04cdeca17f
Pierre Paillere <peyop78@gmail.com>
2017-05-29 15:34:59 +0200

    cleaned pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,34 +7,4 @@ class PagesController < ApplicationController
     @last_events = Event.last(3)
     @last_performances = Performance.last(3)
   end
-
-  def search
-    search_type = params[:search_type]
-    if search_type == 'Events'
-      search_events
-    elsif search_type == 'Places'
-      search_places
-    elsif search_type == 'Artists'
-      search_artists
-    end
-  end
-
-  def search_artists
-    params[:performance] = {
-      date: Date.now,
-      address: params[:address],
-      event_type: 'Music'
-    }
-    redirect_to performances_path
-  end
-
-  def search_events
-    @events = Event.all
-    redirect_to events_path
-  end
-
-  def search_places
-    @places = Place.all
-    redirect_to places_path
-  end
 end
