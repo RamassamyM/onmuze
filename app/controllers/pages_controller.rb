5e51729276e419af0f95933e67f38435a1105188
Pierre Paillere <peyop78@gmail.com>
2017-05-29 15:15:14 +0200

    add inline find artist form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    # @genres = Genre.all.map(&:event_type).uniq
+    @genres = Genre.all.map(&:event_type).uniq
     @last_places = Place.last(3)
     @last_events = Event.last(3)
     @last_performances = Performance.last(3)
