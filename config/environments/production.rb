30366e34501dec804ad213c8c9d5594fb7e5b2e8
Pierre Paillere <peyop78@gmail.com>
2017-05-30 15:25:06 +0200

    fixed sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,12 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'onmuze.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.onmuze.com' }
   config.action_mailer.perform_deliveries = true
   # SendGrid config
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'onmuze.herokuapp.com',
+    domain: 'onmuze.com',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
