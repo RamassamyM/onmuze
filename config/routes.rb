fc850366f1f555ba7fdb57459e7bd35c48a3844a
Michael Ramassamy <ramassamy2000@gmail.com>
2017-05-31 23:23:15 +0200

    Configure routes for promotions

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
     resources :proposals, only: %i(create)
   end
   resources :places, only: %i(index show new create update), shallow: true do
-    resources :events, only: %i(index show create update)
+    resources :events, only: %i(index show create update), shallow: true do
+      resources :promotions, only: %i(index create)
+    end
   end
   resources :events, only: %i(create)
   resources :proposals, only: %i(update)
-  resources :promotion, only: %i(show create)
   root 'pages#startdirect'
   get 'home', to: 'pages#home'
   get 'choose', to: 'pages#choose'
