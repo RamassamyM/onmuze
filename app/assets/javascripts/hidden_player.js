426cb72cc6210528221217f3ef90efdf24700b29
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-31 12:22:57 +0200

    Added soundcloud index feature

--- app/assets/javascripts/hidden_player.js
+++ app/assets/javascripts/hidden_player.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   $(".play-btn").click(function(e) {
-    $(".hidden-player").append('<iframe class="smpadded" width="80%" height="0" scrolling="yes" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/207967430&amp;color=e73531&amp;auto_play=true&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_artwork=false"></iframe>')
-    $(this).hide();
-    $(".pause-btn").show();
+    var soundcloud = $(this).data("soundcloud");
+    var id = $(this).data("id");
+    $("#player-" + id).append('<iframe width="80%" height="0" scrolling="yes" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/' + soundcloud + '&amp;color=e73531&amp;auto_play=true&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_artwork=false"></iframe>')
+    $("#play-btn-" + id).toggle();
+    $("#pause-btn-" + id).toggle();
   });
   $(".pause-btn").click(function(e) {
+    var id = $(this).data("id");
     $(".hidden-player").empty();
-    $(this).hide();
-    $(".play-btn").show();
+    $("#pause-btn-" + id).toggle();
+    $("#play-btn-" + id).toggle();
   });
 });
